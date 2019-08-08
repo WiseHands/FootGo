45e4a90a101a1403982d1ae86062e27e78c57afa
Aleksander <tatrun89@gmail.com>
2019-08-08 14:29:02 +0300

    added basic relation test for GAME<->TEAM<->CAPTAIN/PLAYERS

--- src/test/java/ua/lviv/footgo/entity/GameUnitTests.java
+++ src/test/java/ua/lviv/footgo/entity/GameUnitTests.java
@@ -21,6 +21,26 @@ public class GameUnitTests {
 	public static final String TIME = "1920 010203";
 	public static final String LOCATION = "FC SOKIL";
 
+    private static final String TEAM_A_NAME = "ToniTeamA";
+
+    private static final String TEAM_A_CAPTAIN_NAME = "CapA";
+    private static final String TEAM_A_CAPTAIN_PHONE = "CapA@gmail.com";
+    private static final String TEAM_A_CAPTAIN_EMAIL = "30666666666";
+
+    private static final String TEAM_A_PLAYER_ONE_NAME = "Player A1";
+    private static final String TEAM_A_PLAYER_TWO_NAME = "Player A2";
+
+
+
+    private static final String TEAM_B_NAME = "ToniTeamB";
+
+    private static final String TEAM_B_CAPTAIN_NAME = "CapB";
+    private static final String TEAM_B_CAPTAIN_PHONE = "CapB@gmail.com";
+    private static final String TEAM_B_CAPTAIN_EMAIL = "39666666666";
+
+    private static final String TEAM_B_PLAYER_ONE_NAME = "Player B1";
+    private static final String TEAM_B_PLAYER_TWO_NAME = "Player B2";
+
 	@Autowired
 	private TestEntityManager entityManager;
 
@@ -43,6 +63,82 @@ public class GameUnitTests {
 		assertThat(gameFromDB.getGameTime()).isEqualTo(TIME);
 		assertThat(gameFromDB.getLocation()).isEqualTo(LOCATION);
 	}
+
+	@Test
+    public void testTeamRelation() {
+        Team teamA = new Team();
+        teamA.setTeamName(TEAM_A_NAME);
+
+        Captain captainA = new Captain();
+        captainA.setCaptainName(TEAM_A_CAPTAIN_NAME);
+        captainA.setCaptainEmail(TEAM_A_CAPTAIN_EMAIL);
+        captainA.setCaptainPhone(TEAM_A_CAPTAIN_PHONE);
+
+        captainA.setTeam(teamA);
+        teamA.setCaptain(captainA);
+
+        Player player = new Player();
+        player.setPlayerName(TEAM_A_PLAYER_ONE_NAME);
+        player.setTeam(teamA);
+
+        Player player2 = new Player();
+        player2.setPlayerName(TEAM_A_PLAYER_TWO_NAME);
+        player2.setTeam(teamA);
+
+        teamA.addPlayer(player);
+        teamA.addPlayer(player2);
+
+        entityManager.persist(captainA);
+        entityManager.persist(teamA);
+        entityManager.flush();
+
+
+
+
+
+        Team teamB = new Team();
+        teamB.setTeamName(TEAM_B_NAME);
+
+        Captain captainB = new Captain();
+        captainB.setCaptainName(TEAM_B_CAPTAIN_NAME);
+        captainB.setCaptainEmail(TEAM_B_CAPTAIN_EMAIL);
+        captainB.setCaptainPhone(TEAM_B_CAPTAIN_PHONE);
+
+        captainB.setTeam(teamB);
+        teamB.setCaptain(captainB);
+
+        player = new Player();
+        player.setPlayerName(TEAM_B_PLAYER_ONE_NAME);
+        player.setTeam(teamB);
+
+        player2 = new Player();
+        player2.setPlayerName(TEAM_B_PLAYER_TWO_NAME);
+        player2.setTeam(teamB);
+
+        teamB.addPlayer(player);
+        teamB.addPlayer(player2);
+
+        entityManager.persist(captainB);
+        entityManager.persist(teamB);
+        entityManager.flush();
+
+        Game game = new Game();
+        game.setFirstTeam(teamA);
+        game.setSecondTeam(teamB);
+        gameRepository.save(game);
+
+        List<Game> gameListByFirstTeam = gameRepository.findByFirstTeam(teamA);
+        assertThat(gameListByFirstTeam.size()).isEqualTo(1);
+
+        Game gameFromDb = gameListByFirstTeam.get(0);
+        assertThat(gameFromDb.getFirstTeam().getTeamName()).isEqualTo(teamA.getTeamName());
+        assertThat(gameFromDb.getFirstTeam().getPlayers().size()).isEqualTo(teamA.getPlayers().size());
+        assertThat(gameFromDb.getFirstTeam().getCaptain().getCaptainName()).isEqualTo(teamA.getCaptain().getCaptainName());
+
+        assertThat(gameFromDb.getSecondTeam().getTeamName()).isEqualTo(teamB.getTeamName());
+        assertThat(gameFromDb.getSecondTeam().getPlayers().size()).isEqualTo(teamB.getPlayers().size());
+        assertThat(gameFromDb.getSecondTeam().getCaptain().getCaptainName()).isEqualTo(teamB.getCaptain().getCaptainName());
+    }
 
 	@After
 	public void cleanUp() {
