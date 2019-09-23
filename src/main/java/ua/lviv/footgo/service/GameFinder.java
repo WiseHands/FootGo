e10c2dc517159060235acbf88d84331d36018eba
Bohdan Mykolayovych Tsap <bohdaq@gmail.com>
2019-09-24 01:51:43 +0300

    team page sorting games by tours

--- src/main/java/ua/lviv/footgo/service/GameFinder.java
+++ src/main/java/ua/lviv/footgo/service/GameFinder.java
@@ -7,7 +7,9 @@ import ua.lviv.footgo.entity.Game;
 import ua.lviv.footgo.entity.Team;
 import ua.lviv.footgo.repository.GameRepository;
 
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Service
 public class GameFinder {
@@ -21,9 +23,16 @@ public class GameFinder {
         return gameList;
     }
 
-    public List<Game> findAllCompletedGamesForTeam(Team team) {
+    public List<Game> findAllCompletedGamesForTeam(Team team){
         List<Game> gameList = (List<Game>) gameRepository.findByFirstTeamAndIsCompleted(team, true);
         gameList.addAll((List<Game>) gameRepository.findBySecondTeamAndIsCompleted(team, true));
+
+        Collections.sort(gameList, new Comparator<Game>() {
+            public int compare(Game o1, Game o2) {
+                return o1.getTour().getTourNumber().compareTo(o2.getTour().getTourNumber());
+            }
+        });
+
         return gameList;
     }
 }
