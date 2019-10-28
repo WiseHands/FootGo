4b8da6e796e2a50daff1ebb533b4c31ccab5c92f
Taras H <research.010@gmail.com>
2019-10-28 18:15:25 +0200

    main head title bg fix

--- src/main/webapp/leafletMaps.js
+++ src/main/webapp/leafletMaps.js
@@ -1,10 +1,9 @@
 var map = L.map('map', {
    dragging: !L.Browser.mobile,
    touchZoom: true
-}).setView([49.823774, 23.953927], 13);
+}).setView([49.823774, 23.953927], 15);
 L.tileLayer('https://api.maptiler.com/maps/darkmatter/{z}/{x}/{y}.png?key=gazyc6Cl99WzG1oKAzUT', {
-     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-     maxZoom: 18
+     maxZoom: 20
 }).addTo(map);
 var marker = L.marker([49.82307, 23.95303]).addTo(map);
 var map_link = "https://www.google.com/maps/place/ФК+«Львів»/@49.8239124,23.9559869,15z/data=!4m5!3m4!1s0x0:0x5be70be9b04dfcfe!8m2!3d49.8232753!4d23.9529397?hl=uk";
