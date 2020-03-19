3dfedc1535ca44ba74ae8d1ceeaefb42cfe47cb4
TarasH1 <research010@gmail.com>
2020-03-19 17:09:02 +0200

    league create page update wip

---
+++ src/main/webapp/submissionLeague.js
@@ -0,0 +1,16 @@
+const path = location.toString();
+let url = new URL(path);
+
+let searchParams = new URLSearchParams(url.search);
+
+const id = searchParams.get('uuid');
+
+function submissionData(event) {
+    let leagueName = document.getElementById('leagueName').value;
+    console.log(leagueName);
+
+    let radios = document.querySelectorAll('input[type=checkbox]:checked');
+    radios.forEach((radio) => {
+        console.log(radio.getAttribute('value'));
+    });
+}
