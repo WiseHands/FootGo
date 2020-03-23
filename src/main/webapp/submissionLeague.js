972a566a4dcd5367af907e524893659bb97fdf39
TarasH1 <research010@gmail.com>
2020-03-23 13:55:28 +0200

    league create request/error handling

--- src/main/webapp/submissionLeague.js
+++ src/main/webapp/submissionLeague.js
@@ -5,53 +5,42 @@ let seasonId = url.match(regex);
 
 function submissionData(event) {
     let leagueName = document.getElementById('leagueName').value;
-    if(leagueName) {
-        console.log(leagueName);
 
-        //let radios = document.querySelectorAll('input[type=checkbox]:checked');
+    const checkEmpty = document.getElementById("leagueName");
+    if (checkEmpty.value == "" && checkEmpty.value.length == 0) {
+        document.getElementById('showInputAddLeagueNameError').style.display = "block";
+        checkEmpty.classList.add("league-name-error");
+        return false;
+    }
+    if(leagueName) {
         function getCheckedValues() {
             return Array.from(document.querySelectorAll('input[type="checkbox"]'))
             .filter((checkbox) => checkbox.checked)
             .map((checkbox) => checkbox.value);
         }
-        //console.log(getCheckedValues());
 
-/*        radios.forEach((radio) => {
-            console.log(radio.getAttribute('value'));
-        });*/
-
-
-        let leagueDataJson = {"name" : leagueName, "teamList" : getCheckedValues()};
-        console.log(leagueDataJson);
+        let leagueDataJsonObj = {"name" : leagueName, "teamList" : getCheckedValues()};
+        let jsonLeagueInString = JSON.stringify(leagueDataJsonObj);
 
         let apiUrl = '/api/season/' + seasonId + '/league';
         fetch(apiUrl, {
             method: 'POST',
-            body: leagueDataJson,
+            body: jsonLeagueInString,
             headers: {
               'Content-Type': 'application/json',
             }
         }).then(function(response) {
             console.log(response);
             if(response.ok) {
-/*                document.getElementById('cardsModal').style.display = "none";
-                location.pathname = location.pathname;*/
+                location.pathname = location.pathname;
             }
-            if(response.status === 403) {
-/*                document.getElementById('cardsModal').style.display = "none";
-                alert('Максимальна кількість карток для гравця');*/
+            else {
+                alert('Помилка при створенні ліги');
             }
             return  response.json();
         }).then(function(data) {
             console.log(data);
         })
     }
-
-/*       const checkEmpty = document.getElementById("leagueName");
-       if (checkEmpty.value == "" && checkEmpty.value.length == 0) {
-           //document.getElementById('showInputLeagueNameError').style.display = "block";
-           //document.getElementById('cardMinute').style.cssText = "border-color: #fc2c2c; border-style: solid; border-width: thin";
-           return false;
-       }*/
 
 }
