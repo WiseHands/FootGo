ef5b449631d455875210cdb1b6fa3f535ebc1af5
TarasH1 <research010@gmail.com>
2020-05-18 14:48:55 +0300

    + Instagram gallery hotfix

--- src/main/webapp/widget.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function() {
-    let adw = document.querySelectorAll("a[href='https://elfsight.com/instagram-feed-instashow/?utm_source=websites&utm_medium=clients&utm_content=instagram-feed&utm_term=localhost:8080&utm_campaign=free-widget']")[0];
-    adw.style.display = "none";
-    adw.remove();
-},3500);
