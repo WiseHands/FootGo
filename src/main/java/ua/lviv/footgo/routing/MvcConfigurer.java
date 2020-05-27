0312ab9b813fc0dd03b29db44971032f105a9603
TarasH1 <research010@gmail.com>
2020-05-27 18:43:40 +0300

    user authorization wip

--- src/main/java/ua/lviv/footgo/routing/MvcConfigurer.java
+++ src/main/java/ua/lviv/footgo/routing/MvcConfigurer.java
@@ -2,6 +2,7 @@ package ua.lviv.footgo.routing;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +11,12 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class MvcConfigurer implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
 
     @Bean
     public LocaleResolver localeResolver() {
