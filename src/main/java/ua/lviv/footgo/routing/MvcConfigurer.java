3fc97f98752e6ebeedb33c5238b46eb4b2e03b5a
TarasH1 <research010@gmail.com>
2020-05-28 19:10:53 +0300

    admin authorization wip

--- src/main/java/ua/lviv/footgo/routing/MvcConfigurer.java
+++ src/main/java/ua/lviv/footgo/routing/MvcConfigurer.java
@@ -2,7 +2,6 @@ package ua.lviv.footgo.routing;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,12 +10,6 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class MvcConfigurer implements WebMvcConfigurer {
-
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
-    }
 
     @Bean
     public LocaleResolver localeResolver() {
@@ -32,4 +25,6 @@ public class MvcConfigurer implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeInterceptor());
     }
+
+
 }
--- src/main/java/ua/lviv/footgo/secure/WebSecurityConfig.java
+++ src/main/java/ua/lviv/footgo/secure/WebSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,36 +13,58 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() throws Exception {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        manager.createUser(User
+                .withUsername("admin")
+                .password(encoder().encode("adminPass"))
+                .roles("ADMIN")
+                .build());
+
+        return manager;
     }
 
     @Bean
-/*    public PasswordEncoder passwordEncoder() {
+    public static PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
-    }*/
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
     }
 
+    @Configuration
+    public static class App1ConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        public App1ConfigurationAdapter() {
+            super();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/admin*")
+                    .authorizeRequests()
+                    .anyRequest()
+                    .hasRole("ADMIN")
+
+                    .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .loginProcessingUrl("/admin_login")
+                    .failureUrl("/login?error=loginError")
+                    .defaultSuccessUrl("/admin")
+
+                    .and()
+                    .logout()
+                    .logoutUrl("/admin_logout")
+                    .logoutSuccessUrl("/admin")
+                    .deleteCookies("JSESSIONID")
+
+                    .and()
+                    .exceptionHandling()
+                    .accessDeniedPage("/403")
+
+                    .and()
+                    .csrf().disable();
+        }
+    }
 }
