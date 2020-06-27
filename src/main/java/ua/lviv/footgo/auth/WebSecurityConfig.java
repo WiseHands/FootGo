c2405633ca79c0298a9f53d50ade0d1a21440dd0
TarasH1 <research010@gmail.com>
2020-06-27 19:47:07 +0300

    spring web security https bug fix

--- src/main/java/ua/lviv/footgo/auth/WebSecurityConfig.java
+++ src/main/java/ua/lviv/footgo/auth/WebSecurityConfig.java
@@ -29,6 +29,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .requiresChannel()
+                .anyRequest()
+                .requiresSecure()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/registration").not().fullyAuthenticated()
 /*                    .anyRequest()
