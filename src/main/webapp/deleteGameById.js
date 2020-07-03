6e40daa087c3346af5cca1e59ed7d0bbd9deaed2
TarasH1 <research010@gmail.com>
2020-07-03 21:48:49 +0300

    Game remove and create inside league

---
+++ src/main/webapp/deleteGameById.js
@@ -0,0 +1,24 @@
+function deleteGameById(event) {
+
+    if(tourId) {
+        let params = '?tourId=' + tourId + '&gameId=' + gameId;
+        let apiUrl = '/api/season/' + seasonId + '/leaguelist/' + leagueId + '/game/' + gameId + params;
+        fetch(apiUrl, {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json',
+            }
+        }).then(function(response) {
+            console.log(response);
+            if(response.ok) {
+                window.location = '/admin/tournament/' + tournamentId + '/season/' + seasonId + '/leaguelist/' + leagueId + '/tour/details' + '?uuid=' + tourId;
+            }
+            else {
+                alert('Помилка при видаленні гри');
+            }
+            return  response.json();
+        }).then(function(data) {
+            console.log(data);
+        })
+    }
+}
