c23458404844340bedc4c9f76557719bed7b5328
TarasH1 <research010@gmail.com>
2020-07-03 21:00:17 +0300

    Game remove and create inside league - WIP

---
+++ src/main/webapp/createNewGameInTour.js
@@ -0,0 +1,25 @@
+function submissionData(event) {
+
+    if(tourId) {
+        let params = '?tourId=' + tourId;
+        let apiUrl = '/api/season/' + seasonId + '/leaguelist/' + leagueId + '/tour/details' + params;
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            }
+        }).then(function(response) {
+            console.log(response);
+            if(response.ok) {
+                location.pathname = location.pathname;
+            }
+            else {
+                alert('Помилка при створенні гри');
+            }
+            return  response.json();
+        }).then(function(data) {
+            console.log(data);
+        })
+    }
+}
+
