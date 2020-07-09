e9fd65c3c183eb93534593e42ee778cf009f6252
TarasH1 <research010@gmail.com>
2020-07-09 20:29:59 +0300

    Top scorers service improvement (now it's shows results by league) WIP

--- src/main/java/ua/lviv/footgo/routing/HttpRequestsController.java
+++ src/main/java/ua/lviv/footgo/routing/HttpRequestsController.java
@@ -175,8 +175,7 @@ public class HttpRequestsController {
         model.addAttribute("secondPlace", results.get(1));
         model.addAttribute("thirdPlace", results.get(2));*/
         model.addAttribute("resultList", results);
-        //List<PlayerGoals> playerGoals = topScorerService.getResults();
-        List<PlayerGoals> playerGoals = topScorerService.getResultsByLeague(leagueId);
+        List<PlayerGoals> playerGoals = topScorerService.getResults();
         model.addAttribute("playerGoals", playerGoals);
         List<Game> games = (List<Game>) gameRepository.findAll();
         Game game = games.get(0);
@@ -331,8 +330,7 @@ public class HttpRequestsController {
         League league = leagueManagementRepository.findById(leagueId).get();
         model.addAttribute("league", league);
 
-        //List<PlayerGoals> playerGoals = topScorerService.getResults();
-        List<PlayerGoals> playerGoals = topScorerService.getResultsByLeague(leagueId);
+        List<PlayerGoals> playerGoals = topScorerService.getResults();
         model.addAttribute("playerGoals", playerGoals);
 
 /*        List<PlayerGoals> playerGoals = topScorerService.getResults();
