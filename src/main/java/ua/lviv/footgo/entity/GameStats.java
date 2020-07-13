2f5d04c77d78786db2baaa8229aa6119195e1330
TarasH1 <research010@gmail.com>
2020-07-13 18:42:36 +0300

    game stats controller

--- src/main/java/ua/lviv/footgo/entity/GameStats.java
+++ src/main/java/ua/lviv/footgo/entity/GameStats.java
@@ -28,4 +28,56 @@ public class GameStats {
     @Column
     int cornerKicks;
 
+    public Long getId() {
+        return id;
+    }
+
+    public int getStrikes() {
+        return strikes;
+    }
+
+    public void setStrikes(int strikes) {
+        this.strikes = strikes;
+    }
+
+    public int getHitsTheTarget() {
+        return hitsTheTarget;
+    }
+
+    public void setHitsTheTarget(int hitsTheTarget) {
+        this.hitsTheTarget = hitsTheTarget;
+    }
+
+    public int getStrikesPastTheGate() {
+        return strikesPastTheGate;
+    }
+
+    public void setStrikesPastTheGate(int strikesPastTheGate) {
+        this.strikesPastTheGate = strikesPastTheGate;
+    }
+
+    public int getFreeKicks() {
+        return freeKicks;
+    }
+
+    public void setFreeKicks(int freeKicks) {
+        this.freeKicks = freeKicks;
+    }
+
+    public int getFouls() {
+        return fouls;
+    }
+
+    public void setFouls(int fouls) {
+        this.fouls = fouls;
+    }
+
+    public int getCornerKicks() {
+        return cornerKicks;
+    }
+
+    public void setCornerKicks(int cornerKicks) {
+        this.cornerKicks = cornerKicks;
+    }
+
 }
