4c0404517b7c983c1a8ddf04bbd365c59dc30c3e
TarasH1 <research010@gmail.com>
2020-07-23 16:02:16 +0300

    match statistics

---
+++ src/main/webapp/matchStatistics.js
@@ -0,0 +1,35 @@
+function percentageForTeamA(teamAValue, teamBValue) {
+    sum = teamAValue + teamBValue;
+    if (teamAValue < sum) {
+        return Math.round((teamAValue / sum) * 100);
+    }
+}
+
+function percentageForTeamB(teamAValue, teamBValue) {
+    sum = teamAValue + teamBValue;
+    if (teamBValue < sum) {
+        return Math.round((teamBValue / sum) * 100);
+    }
+}
+
+function calculatePercentage(resultForTeamA, resultForTeamB, progressBarForTeamA, progressBarForTeamB) {
+    if (resultForTeamA) {
+        progressBarForTeamA.style.width = resultForTeamA + '%';
+        if (resultForTeamA > resultForTeamB) {
+            progressBarForTeamA.classList.add("bar-more");
+            progressBarForTeamB.classList.add("bar-less");
+        }
+        if (resultForTeamA === resultForTeamB) {
+            progressBarForTeamA.classList.add("bar-less");
+            progressBarForTeamB.classList.add("bar-less");
+        }
+    }
+
+    if (resultForTeamB) {
+        progressBarForTeamB.style.width = resultForTeamB + '%';
+        if (resultForTeamB > resultForTeamA) {
+            progressBarForTeamB.classList.add("bar-more");
+            progressBarForTeamA.classList.add("bar-less");
+        }
+    }
+}
